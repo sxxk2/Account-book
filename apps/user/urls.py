63c0adb57dbc134388df86fe66e6fcbaed1fa060
sxxk2 <sxxklee@gmail.com>
2023-02-04 02:19:19 +0900

    feat: 로그아웃 (#14)

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.user.views import SignInView, SignUpView
+from apps.user.views import SignInView, SignOutView, SignUpView
 
 app_name = "user"
 
 urlpatterns = [
     path("/signup", SignUpView.as_view(), name="user_signup"),
     path("/signin", SignInView.as_view(), name="user_signin"),
+    path("/signout", SignOutView.as_view(), name="user_signout"),
 ]
