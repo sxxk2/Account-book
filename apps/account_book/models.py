3a42280da58937554dbc0474bf5d1d27e7bcee89
sxxk2 <sxxklee@gmail.com>
2023-02-04 04:35:07 +0900

    add: account_book 앱 모델링 (#16)

--- apps/account_book/models.py
+++ apps/account_book/models.py
@@ -1,1 +1,33 @@
-# from django.db import models
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from apps.utils.timestamp import TimeStampedModel
+
+
+class AccountBook(TimeStampedModel):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    balance = models.IntegerField(default=0)
+    is_active = models.BooleanField("삭제 여부", default=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "account_books"
+
+
+class AccountBookRecord(TimeStampedModel):
+    account_book = models.ForeignKey(to=AccountBook, on_delete=models.CASCADE)
+    amount = models.IntegerField("금액")
+    description = models.CharField(max_length=100)
+    date = models.DateField()
+    is_active = models.BooleanField("삭제 여부", default=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f"{self.description} : {self.amount}"
+
+    class Meta:
+        db_table = "account_book_records"
