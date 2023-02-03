2242f8a67decb12d80c985c0bf2b2518daa50e75
sxxk2 <sxxklee@gmail.com>
2023-02-04 02:22:03 +0900

    feat: 토큰 재발급 (#14)

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.user.views import SignInView, SignOutView, SignUpView
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path("/signup", SignUpView.as_view(), name="user_signup"),
     path("/signin", SignInView.as_view(), name="user_signin"),
     path("/signout", SignOutView.as_view(), name="user_signout"),
+    path("/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
